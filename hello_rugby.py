d190c8eb41ca0dc6141143d64eacff54d4c31884
Eric Picot <eric.picot@hotmail.fr>
2019-03-03 20:38:12 +0100

    importing hello_rugby.py

---
+++ hello_rugby.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  3 20:23:07 2019
+
+@author: ericpicot
+"""
+
+import pandas as pd
+import numpy as np
+
+print("hello world")
+
+a =[x**2 for x in range(5)]
